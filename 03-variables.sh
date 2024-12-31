e1dc462787a9b811ed1beb95812ef9847ff4f721
Raghu Prasad Voleti <jack>
2024-12-31 17:43:29 +0530

    best practice of variables

---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+
+echo "$PERSON1:: How are u"
+echo "$PERSON2:: I am fine"
+echo "$PERSON1:: How the things are doing"
+echo "$PERSON2:: Awesome"
+echo "$PERSON1:: Can we catch up today"
+echo "$PERSON2:: Sure Jack"
+echo "$PERSON1:: Thanks Smart!"
+
