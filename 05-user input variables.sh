0ca2a8a40c4924783e47be33b70868bf4a006c2a
Raghu Prasad Voleti <jack>
2024-12-31 18:30:37 +0530

    best practice of variables

---
+++ 05-user input variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username::"
+
+read -s USERNAME
+
+echo "username is entered:: $USERNAME"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "password is entered"
