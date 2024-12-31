fdbb8a86906935c039b0e53982af2542a255330b
Raghu Prasad Voleti <jack>
2024-12-31 18:53:31 +0530

    final

--- 05-user input variables.sh
+++ 05-user input variables.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-echo "plese enter your username::"
+echo "please enter your username?"
 read USERNAME
 echo "username is entered $USERNAME"
+
+
+echo "please enter your password::"
+read -s PASSWORD
