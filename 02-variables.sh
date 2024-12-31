3ee99f9e9efa5401e78726873ae0a214bc4e89a1
Raghu Prasad Voleti <jack>
2024-12-31 17:24:37 +0530

    variable message

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#this is variable shellscript
+
+echo "Jack:: How are u"
+echo "Smart:: I am fine"
+echo "Jack:: How the things are doing"
+echo "Smart:: Awesome"
+echo "Jack:: Can we catch up today"
+echo "Smart:: Sure Jack"
+echo "Jack:: Thanks Smart!"
