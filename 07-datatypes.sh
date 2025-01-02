c6155167fbe0a63349392f90c82eb28881236dbf
Raghu Prasad Voleti <jack>
2025-01-02 16:46:27 +0530

    message

---
+++ 07-datatypes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUMBER=$1
+NUMBER=$2
+NUMBER=$3
+
+TIMESTAMP=$(date)
+echo "timestamp is: $TIMESTAMP"
+
+SUM=$($NUMBER1+$NUMBER2+$NUMBER3)
+echo "sum of $NUMBER1 and $NUMBER2 and $NUMBER3 is: $SUM"
