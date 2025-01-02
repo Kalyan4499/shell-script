12d050a56d36e0b12aafda261f8280e330847e7d
Raghu Prasad Voleti <jack>
2025-01-02 17:08:19 +0530

    message

--- 08.functions.sh
+++ 08.functions.sh
@@ -3,8 +3,7 @@
 NUMBER=$1
 
 if [ $NUMBER -gt 100 ]
-then
-echo "number is greater then 100"
-else
-echo "number is lesser then 100"
-fi
+   then
+   echo "number is greater then 100"
+   else
+   echo "number is lesser then 100"
